36bf95796c0e0b9a5927328789aa5306ec4f265e
BigOsDev <bigosdev0@gmail.com>
2017-11-01 23:19:00 +0100

    First working identityserver with api

---
+++ src/Spider.IdentityServer/Clients.cs
@@ -0,0 +1,83 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spider.IdentityServer
+{
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client> {
+            // new Client
+            //{
+            //    ClientId = "apiClient",
+            //    ClientName = "MVC Client",
+            //    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+
+            //    ClientSecrets =
+            //        {
+            //            new Secret("secret".Sha256())
+            //        },
+
+            //    RedirectUris = { "http://localhost:64343/signin-oidc" },
+            //    PostLogoutRedirectUris = { "http://localhost:64343/signout-callback-oidc" },
+
+            //    AllowedScopes =
+            //        {
+            //            IdentityServerConstants.StandardScopes.OpenId,
+            //            IdentityServerConstants.StandardScopes.Profile,
+            //            "api1"
+            //        },
+            //    AllowOfflineAccess = true,
+            //     AllowedCorsOrigins = new List<string>
+            //    {
+            //        "https://localhost:4200",
+            //        "http://localhost:4200",
+            //        "http://localhost:64343",
+            //    },
+            //},
+            //new Client {
+            //    ClientId = "oauthClient",
+            //    ClientName = "Example Client Credentials Client Application",
+            //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+            //    ClientSecrets = new List<Secret> {
+            //        new Secret("superSecretPassword".Sha256())},
+            //    AllowedScopes = new List<string> {"customAPI.read","api1"}
+            //},
+            new Client {
+                ClientId = "angularclientidtokenonly",
+                ClientName = "angularclientidtokenonly",
+                AccessTokenType = AccessTokenType.Reference,
+                AllowedGrantTypes = GrantTypes.Implicit,
+                RequireConsent = false,
+                AllowAccessTokensViaBrowser = true,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "api1",
+                    "role",
+                    "customAPI.write"
+                },
+                RedirectUris = new List<string> {"http://localhost:4200"},
+                PostLogoutRedirectUris = new List<string> { "http://localhost:4200" },
+                AllowedCorsOrigins = new List<string>
+                {
+                    "https://localhost:4200",
+                    "http://localhost:4200",
+                    "http://localhost:64343",
+                },
+            }
+        };
+        }
+    }
+}
---
+++ src/Spider.IdentityServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Spider.IdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:44318/")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
